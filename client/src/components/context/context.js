57a9bffb73c364088909dbc4c7301e372bcd9de2
Piyush-t24 <piyushiitian2023@gmail.com>
2025-11-10 01:46:26 +0530

    refactor(client): remove old component structure (moved to features folder)

--- client/src/components/context/context.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-export const ContextMuseum = createContext()
-export const ContextCheckLogin = createContext()
-export const ContextConfirmOrder = createContext()
-export const ContextUserInfo = createContext()

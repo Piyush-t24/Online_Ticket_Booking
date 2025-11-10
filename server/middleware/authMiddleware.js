4c6994239b1d70fafa1407d0803280d8e3741d43
Piyush-t24 <piyushiitian2023@gmail.com>
2025-11-11 00:13:57 +0530

    feat(server): update authentication middleware

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -7,7 +7,7 @@ const authorModel = require('../db/models/authorModel')
 
 module.exports.authAdmin = async (req, res, next) => {
     try {
-        // Extract token from cookies or Authorization header
+        
         const token = req.cookies.token || (req.header('Authorization') && req.header('Authorization').split(' ')[1]);
         if (!token) {
             return res.status(401).send('<script>alert("Please log in again"); window.location.href="/";</script>');
@@ -80,7 +80,6 @@ module.exports.authAuthor = async (req, res, next) => {
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
         }
-
         // Verify if the token is valid
         const decode = jwt.verify(token, process.env.JWT_SECRET);
 
